b5c4a4ddc1ee3716b1339de83ed365f5551d3571
Flavius Stroe <stroe.flavius@gmail.com>
2019-10-26 16:42:19 +0100

    expanding the EDA class

--- random_daily_image.py
+++ random_daily_image.py
@@ -2,7 +2,7 @@ from api import NasaApod
 
 if __name__ == "__main__":
     Nasa = NasaApod()
-    user_input = input("Would you like to open the image? [y/n]: ")
+    user_input = input('Would you like to open the image? [y/n]: ')
     if user_input == 'y':
         Nasa.open_daily_image()
     else:
